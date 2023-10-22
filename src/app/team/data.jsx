0708d8e7d10141ee787295228121d093bfacc8ba
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-10-22 07:42:58 +0530

    changes in team section

--- src/app/team/data.jsx
+++ src/app/team/data.jsx
@@ -1,32 +1,5 @@
 export const ThirdYear=[
     {
-        id:1,
-        name:'Aditya Tyagi',
-        role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHwILiTLvOAvQ/profile-displayphoto-shrink_800_800/0/1684496712938?e=1703116800&v=beta&t=zDPQffDPCVAp9Ds4ETALXHBiIfcGDQK7Rmo_WPS06AI',
-        description:'MERN STACK WEB DEVELOPER',
-        linkedin:'https://www.linkedin.com/in/adityatyagiav/',
-        link:'https://www.adityatyagi.me/'
-    },
-    {
-        id:2,
-        name:'Dhruv Gajwani',
-        role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/C5603AQEvS9KSB2Dh_w/profile-displayphoto-shrink_400_400/0/1653045839743?e=1703116800&v=beta&t=645bGae1oHUQxTSbWIacGvBaX_aUOb6A1IQ-sFcgBGk',
-        description:'Android Developer and tech enthusiast',
-        linkedin:'https://www.linkedin.com/in/dhruv-gajwani-72346a23b/',
-        link:''
-    },
-    {
-        id:3,
-        name:'Dev Varshney',
-        role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/D4D03AQFGfVFjSkHVYQ/profile-displayphoto-shrink_400_400/0/1665023125593?e=1703116800&v=beta&t=6qkhQlAG_v1NxDURX8T3zFHVmKmMN1wUfrH8itLjzB8',
-        description:'DSA and CP Enthusiast',
-        linkedin:'https://www.linkedin.com/in/dhruv-gajwani-72346a23b/',
-        link:''
-    },
-    {
         id:4,
         name:'Abhinav Chaudhary',
         role:'Coordinator',
@@ -34,6 +7,42 @@ export const ThirdYear=[
         description:'UI/UX Designer',
         linkedin:'https://www.linkedin.com/in/abhinav-chaudhary-62349a22b/',
         link:''
+    },
+    {
+        id:11,
+        name:'Abhishek Verma',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQEhg4ZBLQX-vA/profile-displayphoto-shrink_400_400/0/1672057309815?e=1703116800&v=beta&t=W_EtfbhTvEGHofY6SU98OKzdhRvzcyrEj_MLztiu5FE',
+        description:'DSA and CP Enthusiast',
+        linkedin:'https://www.linkedin.com/in/abhishek-verma-b39878228/',
+        link:''
+    },
+    {
+        id:17,
+        name:'Abhishek Rai',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHNkwdwcoRwuw/profile-displayphoto-shrink_400_400/0/1668834257142?e=1703721600&v=beta&t=0jRs6yEevtHydDuYGxyQs1F7CP_JRsaKHDIEksx7Ry4',
+        description:'DSA and CP Enthusiast',
+        linkedin:'https://www.linkedin.com/in/abhishek-rai-692397227/',
+        link:''
+    },
+    {
+        id:1,
+        name:'Aditya Tyagi',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHwILiTLvOAvQ/profile-displayphoto-shrink_800_800/0/1684496712938?e=1703116800&v=beta&t=zDPQffDPCVAp9Ds4ETALXHBiIfcGDQK7Rmo_WPS06AI',
+        description:'MERN STACK WEB DEVELOPER',
+        linkedin:'https://www.linkedin.com/in/adityatyagiav/',
+        link:'https://github.com/adityatyagi-av'
+    },
+    {
+        id:15,
+        name:'Aryat Srivastava',
+        role:'Coordinator',
+        imageUrl:'/3rdyear/aryat.jpg',
+        description:'Web Developer',
+        linkedin:'https://www.linkedin.com/in/aryat-srivastava-23a765242/',
+        link:'https://github.com/aryat10'   
     },
     {
         id:5,
@@ -45,12 +54,39 @@ export const ThirdYear=[
         link:''
     },
     {
-        id:6,
-        name:'Vikas Kumar',
+        id:3,
+        name:'Dev Varshney',
         role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/C4D03AQFlLMURb7sKxQ/profile-displayphoto-shrink_400_400/0/1655796639359?e=1703116800&v=beta&t=7GDdzIurH1R25Bjo10mwCn8M4nrMLqmf2IBFoAe4TA4',
-        description:'AI/ML and AR/VR Lead',
-        linkedin:'https://www.linkedin.com/in/vikaskumar142001/',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQFGfVFjSkHVYQ/profile-displayphoto-shrink_400_400/0/1665023125593?e=1703116800&v=beta&t=6qkhQlAG_v1NxDURX8T3zFHVmKmMN1wUfrH8itLjzB8',
+        description:'DSA and CP Enthusiast',
+        linkedin:'https://www.linkedin.com/in/varshneydev27/',
+        link:''
+    },
+    {
+        id:2,
+        name:'Dhruv Gajwani',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/C5603AQEvS9KSB2Dh_w/profile-displayphoto-shrink_400_400/0/1653045839743?e=1703116800&v=beta&t=645bGae1oHUQxTSbWIacGvBaX_aUOb6A1IQ-sFcgBGk',
+        description:'Android Developer and tech enthusiast',
+        linkedin:'https://www.linkedin.com/in/dhruv-gajwani-72346a23b/',
+        link:''
+    },
+    {
+        id:8,
+        name:'Divyanshi Srivastava',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHNNIZZNFwo3w/profile-displayphoto-shrink_400_400/0/1696449287590?e=1703116800&v=beta&t=KjwlmMbVrj962fbY5B19eeYpw3kBPTQzuikgYIQd_gg',
+        description:'Social Media Handler',
+        linkedin:'https://www.linkedin.com/in/divyanshi-srivastava-775925222/',
+        link:''
+    },
+    {
+        id:16,
+        name:'Keshav Tyagi',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHzpwkk_o9Vng/profile-displayphoto-shrink_400_400/0/1665625520356?e=1703721600&v=beta&t=FjIpD33gz_uQbEvttH6mhaDsamK04zjgkpkGrpYRLXI',
+        description:'App Developer',
+        linkedin:'https://www.linkedin.com/in/keshav-tyagi-55a309236/',
         link:''
     },
     {
@@ -63,16 +99,6 @@ export const ThirdYear=[
         link:''
     },
     {
-        id:8,
-        name:'Divyanshi Srivastava',
-        role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHNNIZZNFwo3w/profile-displayphoto-shrink_400_400/0/1696449287590?e=1703116800&v=beta&t=KjwlmMbVrj962fbY5B19eeYpw3kBPTQzuikgYIQd_gg',
-        description:'Social Media Handler',
-        linkedin:'https://www.linkedin.com/in/divyanshi-srivastava-775925222/',
-        link:''
-    },
-    
-    {
         id:9,
         name:'Saurabh',
         role:'Coordinator',
@@ -80,24 +106,6 @@ export const ThirdYear=[
         description:'AR / VR Lead',
         linkedin:'https://www.linkedin.com/in/saurabh-kashyap-b7ab0b257/',
         link:'https://github.com/saurabh873'
-    },
-    {
-        id:10,
-        name:'Vishakha Bansal',
-        role:'Coordinator',
-        imageUrl:'3rdyear/vishakha.jpg',
-        description:'DSA and CP Enthusiast',
-        linkedin:'https://www.linkedin.com/in/vishakha-bansal-56188b225',
-        link:'https://github.com/Vishakha-Baansal'
-    },
-    {
-        id:11,
-        name:'Abhishek Verma',
-        role:'Coordinator',
-        imageUrl:'https://media.licdn.com/dms/image/D4D03AQEhg4ZBLQX-vA/profile-displayphoto-shrink_400_400/0/1672057309815?e=1703116800&v=beta&t=W_EtfbhTvEGHofY6SU98OKzdhRvzcyrEj_MLztiu5FE',
-        description:'DSA and CP Enthusiast',
-        linkedin:'https://www.linkedin.com/in/abhishek-verma-b39878228/',
-        link:''
     },
     {
         id:12,
@@ -109,6 +117,24 @@ export const ThirdYear=[
         link:'https://github.com/shivaniirajput'
     },
     {
+        id:14,
+        name:'Shoaib Ahmad',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQEJJhqchJ-s0Q/profile-displayphoto-shrink_400_400/0/1679477521652?e=1703721600&v=beta&t=bJ9zb5zu0rG6YqPkQqlbglnApGjsxPzAU-bJnzERXdE',
+        description:'Social Media Handler',
+        linkedin:'https://www.linkedin.com/in/shoaib-ahmad-2a0845233/',
+        link:'https://github.com/nottshoaib'
+    },
+    {
+        id:6,
+        name:'Vikas Kumar',
+        role:'Coordinator',
+        imageUrl:'https://media.licdn.com/dms/image/C4D03AQFlLMURb7sKxQ/profile-displayphoto-shrink_400_400/0/1655796639359?e=1703116800&v=beta&t=7GDdzIurH1R25Bjo10mwCn8M4nrMLqmf2IBFoAe4TA4',
+        description:'AI/ML and AR/VR Lead',
+        linkedin:'https://www.linkedin.com/in/vikaskumar142001/',
+        link:''
+    },
+    {
         id:13,
         name:'Vikas Kumar',
         role:'Coordinator',
@@ -117,189 +143,220 @@ export const ThirdYear=[
         linkedin:'https://www.linkedin.com/in/vikas-kumar-48429924b/',
         link:''
     },
+    {
+        id:10,
+        name:'Vishakha Bansal',
+        role:'Coordinator',
+        imageUrl:'3rdyear/vishakha.jpg',
+        description:'DSA and CP Enthusiast',
+        linkedin:'https://www.linkedin.com/in/vishakha-bansal-56188b225',
+        link:'https://github.com/Vishakha-Baansal'
+    },
+    
+    
+    
+    
+
     
 ]
 export const SecondYear=[
+    
     {   
         id:1,
-        name:'Pratyush Sharma',
-        imageUrl:'/2ndyear/pratyush.jpg'
-    },
-    {   
-        id:2,
-        name:'Yash Sharma',
-        imageUrl:'/2ndyear/yash.jpg'
-    },
-    {   
-        id:3,
-        name:'Gaurav Yadav',
-        imageUrl:'/2ndyear/gaurav.jpg'
-    },
-    {   
-        id:4,
-        name:'Ishita Tyagi',
-        imageUrl:'/2ndyear/ishita.jpg'
-    },
-    {   
-        id:5,
-        name:'Archit Aggarwal',
-        imageUrl:'/2ndyear/archit.jpg'
-    },
-    {   
-        id:6,
-        name:'Rohit Verma',
-        imageUrl:'/2ndyear/rohit.jpg'
-    },
-    {   
-        id:7,
-        name:'Ansh Singh',
-        imageUrl:'/2ndyear/ansh.jpg'
-    },
-    {   
-        id:8,
-        name:'Santosh Kumar',
-        imageUrl:'/2ndyear/santosh.jpg'
-    },
-    {   
-        id:9,
-        name:'Ankit Jaiswal',
-        imageUrl:'/2ndyear/ankit.jpg'
-    },
-    {   
-        id:10,
-        name:'Simon Srivastava',
-        imageUrl:'https://media.licdn.com/dms/image/D4D03AQH5u_HuKIIR3g/profile-displayphoto-shrink_400_400/0/1693940496246?e=1703116800&v=beta&t=atXuh3hDR2DU67n2JBGWQGWetLB1Bxs1JqbMk14KtIQ'
-    },
-    {   
-        id:11,
-        name:'Praveer Nandan',
-        imageUrl:'/2ndyear/praveer.jpg'
-    },
-    {   
-        id:12,
-        name:'Rishu Raj',
-        imageUrl:'/2ndyear/rishu.jpg'
-    },
-    {   
-        id:13,
-        name:'Shubh Agarwal',
-        imageUrl:'/2ndyear/shubh.jpg'
-    },
-    {   
-        id:14,
-        name:'Mohd. Sauban',
-        imageUrl:'/2ndyear/sauban.jpg'
-    },
-    {   
-        id:15,
-        name:'Harsh Tyagi',
-        imageUrl:'/2ndyear/harsh.jpg'
-    },
-    {   
-        id:16,
-        name:'Adarsh Tiwari',
-        imageUrl:'/2ndyear/adarsh.jpg'
-    },
-    {   
-        id:17,
-        name:'Krishna Garg',
-        imageUrl:'/2ndyear/krishna.jpg'
-    },
-    {   
-        id:18,
-        name:'Divya Verma',
-        imageUrl:'/2ndyear/divya.jpg'
-    },
-    {   
-        id:19,
-        name:'Shipra Maurya',
-        imageUrl:'/2ndyear/shipra.jpg'
-    },
-    {   
-        id:20,
-        name:'Prasannam Tiwari',
-        imageUrl:'/2ndyear/prasannam.jpg'
-    },
-    {   
-        id:21,
-        name:'Aniket Ray',
-        imageUrl:'/2ndyear/aniket.jpg'
-    },
-    {   
-        id:22,
-        name:'Priyanshu Mishra',
-        imageUrl:'/2ndyear/priyanshu.jpg'
-    },
-    {   
-        id:22,
-        name:'Gungun Jain',
-        imageUrl:'/2ndyear/gungun.jpg'
-    },
-    {   
-        id:23,
-        name:'Ankit kumar Jha',
-        imageUrl:'/2ndyear/ankitjha.jpg'
-    },
-    {   
-        id:24,
-        name:'Harsh Verma',
-        imageUrl:'/2ndyear/harshverma.jpg'
-    },
-    {   
-        id:25,
         name:'Aditya Mohan Gupta',
         imageUrl:'/2ndyear/aditya.jpg'
     },
     {   
-        id:26,
-        name:'Prapti Sharma',
-        imageUrl:'/2ndyear/prapti.jpg'
+        id:2,
+        name:'Adarsh Tiwari',
+        imageUrl:'/2ndyear/adarsh.jpg'
+    },
+    {
+        id:3,
+        name:"Agam Singh",
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQHK9_ycw09RyA/profile-displayphoto-shrink_400_400/0/1675968263461?e=1703721600&v=beta&t=poC5iE9fo9tib47CMWVwqyH3tDQIwpy2l-thwcAB4B4'
     },
     {   
-        id:27,
-        name:'Anuj Sharma',
-        imageUrl:'/2ndyear/anuj.jpg'
+        id:4,
+        name:'Aniket Ray',
+        imageUrl:'/2ndyear/aniket.jpg'
     },
     {   
-        id:28,
+        id:5,
+        name:'Ankit Jaiswal',
+        imageUrl:'/2ndyear/ankit.jpg'
+    },
+    {   
+        id:6,
         name:'Ankit Singh',
         imageUrl:'/2ndyear/ankitsingh.jpg'
     },
     {   
-        id:29,
-        name:'Shruti Yadav',
-        imageUrl:'/2ndyear/shruti.jpg'
+        id:7,
+        name:'Ankit kumar Jha',
+        imageUrl:'/2ndyear/ankitjha.jpg'
     },
     {   
-        id:30,
-        name:'Tripti Garg',
-        imageUrl:'/2ndyear/tripti.jpg'
+        id:8,
+        name:'Ansh Singh',
+        imageUrl:'/2ndyear/ansh.jpg'
     },
     {   
-        id:31,
-        name:'Shobhika',
-        imageUrl:'/2ndyear/shobhika.jpg'
+        id:9,
+        name:'Anuj Sharma',
+        imageUrl:'/2ndyear/anuj.jpg'
     },
     {   
-        id:32,
-        name:'Mukul Aggarwal',
-        imageUrl:'/2ndyear/mukul.jpg'
+        id:10,
+        name:'Archit Agarwal',
+        imageUrl:'/2ndyear/archit.jpg'
     },
     {   
-        id:33,
-        name:'Ayush Aggarwal',
+        id:11,
+        name:'Ayush Agarwal',
         imageUrl:'/2ndyear/ayush.jpg'
     },
     {   
-        id:34,
+        id:12,
+        name:'Divya Verma',
+        imageUrl:'/2ndyear/divya.jpg'
+    },
+    {   
+        id:13,
+        name:'Gaurav Yadav',
+        imageUrl:'/2ndyear/gaurav.jpg'
+    },
+    {   
+        id:14,
+        name:'Gungun Jain',
+        imageUrl:'/2ndyear/gungun.jpg'
+    },
+    {   
+        id:15,
         name:'Hardik Garg',
         imageUrl:'/2ndyear/hardik.jpg'
     },
     {   
-        id:35,
+        id:16,
+        name:'Harsh Tyagi',
+        imageUrl:'/2ndyear/harsh.jpg'
+    },
+    {   
+        id:17,
+        name:'Harsh Verma',
+        imageUrl:'/2ndyear/harshverma.jpg'
+    },
+    {   
+        id:18,
+        name:'Ishita Tyagi',
+        imageUrl:'/2ndyear/ishita.jpg'
+    },
+    {   
+        id:19,
+        name:'Krishna Kumar',
+        imageUrl:'/2ndyear/krishna.jpg'
+    },
+    {   
+        id:20,
+        name:'Mohd. Sauban',
+        imageUrl:'/2ndyear/sauban.jpg'
+    },
+    {   
+        id:21,
+        name:'Mukul Agarwal',
+        imageUrl:'/2ndyear/mukul.jpg'
+    },
+    {   
+        id:22,
         name:'Neelabh Srivastava',
         imageUrl:'/2ndyear/neelabh.jpg'
     },
+    {
+        id:23,
+        name:'Piyush Varshney',
+        imageUrl:'/2ndyear/piyush.jpg'
+    },
+    {   
+        id:24,
+        name:'Prapti Sharma',
+        imageUrl:'/2ndyear/prapti.jpg'
+    },
+    {   
+        id:25,
+        name:'Prasannam Tiwari',
+        imageUrl:'/2ndyear/prasannam.jpg'
+    },
+    {   
+        id:26,
+        name:'Pratyush Sharma',
+        imageUrl:'/2ndyear/pratyush.jpg'
+    },
+    {   
+        id:27,
+        name:'Praveer Nandan',
+        imageUrl:'/2ndyear/praveer.jpg'
+    },
+    {   
+        id:28,
+        name:'Priyanshu Mishra',
+        imageUrl:'/2ndyear/priyanshu.jpg'
+    },
+    {   
+        id:29,
+        name:'Rishu Raj',
+        imageUrl:'/2ndyear/rishu.jpg'
+    },
+    {   
+        id:30,
+        name:'Rohit Verma',
+        imageUrl:'/2ndyear/rohit.jpg'
+    },
+    {   
+        id:31,
+        name:'Santosh Kumar',
+        imageUrl:'/2ndyear/santosh.jpg'
+    },
+    {   
+        id:32,
+        name:'Shobhika',
+        imageUrl:'/2ndyear/shobhika.jpg'
+    },
+    {   
+        id:33,
+        name:'Shruti Yadav',
+        imageUrl:'/2ndyear/shruti.jpg'
+    },
+    {   
+        id:34,
+        name:'Shipra Maurya',
+        imageUrl:'/2ndyear/shipra.jpg'
+    },
+    {   
+        id:35,
+        name:'Shubh Agarwal',
+        imageUrl:'/2ndyear/shubh.jpg'
+    },
+    {   
+        id:36,
+        name:'Simon Srivastava',
+        imageUrl:'https://media.licdn.com/dms/image/D4D03AQH5u_HuKIIR3g/profile-displayphoto-shrink_400_400/0/1693940496246?e=1703116800&v=beta&t=atXuh3hDR2DU67n2JBGWQGWetLB1Bxs1JqbMk14KtIQ'
+    },
+    {   
+        id:37,
+        name:'Tripti Garg',
+        imageUrl:'/2ndyear/tripti.jpg'
+    },
+    {   
+        id:38,
+        name:'Yash Sharma',
+        imageUrl:'/2ndyear/yash.jpg'
+    },
+    
+  
+    
+    
+    
+    
     
     
 
