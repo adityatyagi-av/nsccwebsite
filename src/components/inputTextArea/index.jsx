5d42328f489c07254b372483dcb40d6215de90be
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-10-24 21:57:58 +0530

    registration page updated

--- src/components/inputTextArea/index.jsx
+++ src/components/inputTextArea/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InputTextArea = ({value,label,placeHolder,formikTouched,formikError,formikChange,formikBlur,formikValue}) => {
   return (
     <div className='mb-4'>
-        <label for={value} className="block mb-2 text-sm font-medium text-gray-900 ">{label} {formikTouched && formikError ? (
+        <label htmlFor={value} className="block mb-2 text-sm font-medium text-gray-900 ">{label} {formikTouched && formikError ? (
           <span className=' text-xs text-red-500 mb-.5'>{`(${formikError}*)`}</span>
         ) : null} </label>
         <textarea 
