de86ea7dddea013992dda88afecb8efe3989c31a
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-10-25 10:49:35 +0530

    added page modification

---
+++ src/components/home/projects/data.jsx
@@ -0,0 +1,75 @@
+const topaimlprojects=[
+    {
+        id:1,
+        title:'Satellite    Image Segmentation',
+        developer:'Vikas Kumar',
+        projectImage:'/projects/aiml/satellite.jpg',
+        imageUrl:'https://media.licdn.com/dms/image/C4D03AQFlLMURb7sKxQ/profile-displayphoto-shrink_400_400/0/1655796639359?e=1703116800&v=beta&t=7GDdzIurH1R25Bjo10mwCn8M4nrMLqmf2IBFoAe4TA4',
+        profileLink:'https://www.linkedin.com/in/vikaskumar142001/',
+        projectLink:'https://www.kaggle.com/code/vikasgenisyss/model',
+    },
+    {
+        id:2,
+        title:'Movie Recommendations System',
+        developer:'Vikas Kumar',
+        projectImage:'/projects/aiml/movie.jpg',
+        imageUrl:'https://media.licdn.com/dms/image/C4D03AQFlLMURb7sKxQ/profile-displayphoto-shrink_400_400/0/1655796639359?e=1703116800&v=beta&t=7GDdzIurH1R25Bjo10mwCn8M4nrMLqmf2IBFoAe4TA4',
+        profileLink:'https://www.linkedin.com/in/vikaskumar142001/',
+        projectLink:'https://github.com/VikasKumar2001/Movie-Recommendation-System',
+    },
+    {
+        id:3,
+        title:'Resume Parser',
+        developer:'Vikas Kumar',
+        projectImage:'/projects/aiml/resume.jpg',
+        imageUrl:'https://media.licdn.com/dms/image/C4D03AQFlLMURb7sKxQ/profile-displayphoto-shrink_400_400/0/1655796639359?e=1703116800&v=beta&t=7GDdzIurH1R25Bjo10mwCn8M4nrMLqmf2IBFoAe4TA4',
+        profileLink:'https://www.linkedin.com/in/vikaskumar142001/',
+        projectLink:'https://github.com/VikasKumar2001/CodeClause_Resume_Parser',
+    },
+    
+]
+const toparvrprojects=[
+    {
+        id:1,
+        title:'',
+        developer:'',
+        projectImage:'',
+        imageUrl:'',
+        profileLink:'',
+        projectLink:'',
+    }
+]
+const topandroidprojects=[
+    {
+        id:1,
+        title:'',
+        developer:'',
+        projectImage:'',
+        imageUrl:'',
+        profileLink:'',
+        projectLink:'',
+    }
+]
+const topuiuxprojects=[
+    {
+        id:1,
+        title:'',
+        developer:'',
+        projectImage:'',
+        imageUrl:'',
+        profileLink:'',
+        projectLink:'',
+    }
+]
+const topwebprojects=[
+    {
+        id:1,
+        title:'',
+        developer:'',
+        projectImage:'',
+        imageUrl:'',
+        profileLink:'',
+        projectLink:'',
+    }
+]
+export {topaimlprojects,toparvrprojects,topandroidprojects,topwebprojects,topuiuxprojects};
--- src/components/home/projects/index.jsx
+++ src/components/home/projects/index.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from "react"; // Import React
-
+import { topaimlprojects,topandroidprojects,toparvrprojects,topuiuxprojects,topwebprojects } from "./data";
+import CardMapping from "@/components/cardMap";
 
 const Projects = () => {
   const [tab, setTab] = useState(2)
@@ -32,61 +33,27 @@ const Projects = () => {
             </div>
 
             <div className="flex-1 mt-8 lg:mx-12 lg:mt-0">
-                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
+                
 
                     {(tab==1)&&(
-                        <div>
-                        <img className="object-cover w-full rounded-lg h-78 "
-                            src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
-                            alt=""/>
-                        <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize">Best website
-                            collections</h2>
-                        <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase  ">Website</p>
-                    </div>
+                        <CardMapping data={topuiuxprojects}/>
                     )}
                     {(tab==2)&&(
-                        <div>
-                        <img className="object-cover w-full rounded-lg h-78 "
-                            src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
-                            alt=""/>
-                        <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize">Best website
-                            collections</h2>
-                        <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase">Website</p>
-                    </div>
+                        <CardMapping data={topwebprojects}/>
                     )}
                     {(tab==3)&&(
-                        <div>
-                        <img className="object-cover w-full rounded-lg h-78  "
-                            src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
-                            alt=""/>
-                        <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize ">Best website
-                            collections</h2>
-                        <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase ">Website</p>
-                    </div>
+                        <CardMapping data={topandroidprojects}/>
                     )}
                     {(tab==4)&&(
-                        <div>
-                        <img className="object-cover w-full rounded-lg h-78  "
-                            src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
-                            alt=""/>
-                        <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize ">Best 
-                            collections</h2>
-                        <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase ">Website</p>
-                    </div>
+                        <CardMapping data={topaimlprojects}/>
                     )}
                     {(tab==5)&&(
-                        <div>
-                        <img className="object-cover w-full rounded-lg h-78  "
-                            src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
-                            alt=""/>
-                        <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize ">Best ML</h2>
-                        <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase">Website</p>
-                    </div>
+                        <CardMapping data={toparvrprojects}/>
                     )}
 
                     
                     
-                </div>
+                
             </div>
         </div>
     </div>
