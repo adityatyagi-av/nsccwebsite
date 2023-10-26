1fec402012db7e128a11f9241a352d6ed51880f3
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-10-27 00:40:37 +0530

    Added minor changes

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,113 +1,141 @@
-'use client'
-import { useState, useEffect } from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Button } from "@nextui-org/react";
+"use client"
+import React from "react";
+
+import { Navbar, 
+  NavbarBrand, 
+  NavbarContent, 
+  NavbarItem, 
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem, Button} from "@nextui-org/react";
+
 import Image from "next/image.js";
 import Link from "next/link";
-import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
+import { useRouter } from 'next/navigation'
+
 
 export default function App() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
+  const router=useRouter();
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen); // Toggle the menu state
   };
-
   const menuItems = [
+    
     {
-      "id": 1,
-      "link": "/",
-      "name": "Home"
+      "id":1,
+      "link":"/",
+      "name":"Home"
     },
     {
-      "id": 2,
-      "link": "/about",
-      "name": "About"
+      "id":2,
+      "link":"/about",
+      "name":"About"
     },
     {
-      "id": 3,
-      "link": "/projects",
-      "name": "Projects"
+      "id":3,
+      "link":"/projects",
+      "name":"Projects"
     },
     {
-      "id": 4,
-      "link": "/resources",
-      "name": "Resources"
+      "id":4,
+      "link":"/resources",
+      "name":"Resources"
+    },
+    // {
+    //   "id":5,
+    //   "link":"/blogs",
+    //   "name":"Blogs"
+    // },
+    {
+      "id":6,
+      "link":"/team",
+      "name":"Team"
     },
     {
-      "id": 6,
-      "link": "/team",
-      "name": "Team"
+      "id":7,
+      "link":"/contact",
+      "name":"Contact Us"
     },
     {
-      "id": 7,
-      "link": "/contact",
-      "name": "Contact Us"
+      "id":8,
+      "link":"/login",
+      "name":"Login"
     },
-    {
-      "id": 8,
-      "link": "/login",
-      "name": "Login"
-    }
+
   ];
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
-  useEffect(() => {
-    closeMenu();
-
-    return () => {
-      // Clean up any subscriptions or timers here
-    }
-  }, [pathname, searchParams]);
-
   return (
-    <Navbar
-      maxWidth="xl"
-      onMenuOpenChange={setIsMenuOpen}
-      className="shadow-md pb-1.5"
+    <Navbar 
+    maxWidth="xl"
+    onMenuOpenChange={setIsMenuOpen} 
+    className="shadow-md pb-1.5"
     >
-      <NavbarContent className="flex">
+    
+      <NavbarContent className="flex ">
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
         <NavbarBrand className="mb-1.5">
-          <Image src="/nscclogo.png" width={130} height={90} className="mt-3" alt="image" />
+          <Image src="/nscclogo.png" width={130} height={90} className="mt-3" alt="image"/>
+          
         </NavbarBrand>
       </NavbarContent>
+    
 
       <NavbarContent className="hidden sm:flex mt-3 gap-9 mb-2" justify="center">
-        {menuItems.map((item, index) => (
-          <NavbarItem key={item.id}>
-            <Link color="foreground" href={item.link}>
-              {item.name}
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
+        
+      <NavbarItem>
+          <Link color="foreground" href="/" >
+            Home
+          </Link>
+        </NavbarItem>
 
+        <NavbarItem>
+          <Link color="foreground" href="/about">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem >
+          <Link href="/projects" aria-current="page">
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem >
+        <Link href="/resources" aria-current="page">
+          Resources</Link>
+        </NavbarItem>
+        
+        <NavbarItem >
+          <Link href="/team" aria-current="page">
+            Team
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground"  href="/contact">
+            Contact Us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
       <NavbarContent justify="end">
+        
         <NavbarItem>
           <Button as={Link} color="primary" href="/login" variant="flat">
             Login
           </Button>
         </NavbarItem>
       </NavbarContent>
-
       <NavbarMenu>
         {menuItems.map((item, index) => (
-          <NavbarMenuItem key={item.id}>
+          <NavbarMenuItem key={item.id} onClick={toggleMenu}>
             <Button
-              onClick={() => {
-                setIsMenuOpen(false);
-                closeMenu();
-                router.push(item.link);
-              }}
+            onClick={()=>{
+              setIsMenuOpen(false)
+              router.push(`${item.link}`)
+            }
+            }
               color={
                 index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
               }
