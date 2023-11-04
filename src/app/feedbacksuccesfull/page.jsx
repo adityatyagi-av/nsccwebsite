8b0aa3af732acb9d9335ca23405a4b5510da8f85
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-11-04 19:14:39 +0530

    Added feedback forms

---
+++ src/app/feedbacksuccesfull/page.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mx-auto mt-20 max-w-screen-xl'>
+      <title>Form Succesfully Submitted NSCC KIET | Technical Club Of KIET</title>
+      <h2 className=" mx-auto max-w-screen-xl items-center text-center mb-6 font-sans text-3xl  font-bold leading-none tracking-tight text-gray-900 sm:text-4xl ">
+            <span className="relative inline-block">
+            
+              <span className="relative">FEEDBACK FORM<span className=''> SUCCESFULLY </span>SUBMITTED</span>
+            </span>{' '}
+            <br />
+            <br />
+          
+          </h2>
+
+          <div className='ml-[15vw] mr-[15vw] md:ml-[30%] md:mr-[30%] justify-center'>
+          <a target='_blank' href="https://chat.whatsapp.com/DsQfZvSFUiC5izXf6owWGE" className='bg-green-400 border rounded-lg flex items-center justify-center gap-2 max-w-[20rem] max-h-20 h-16 text-2xl my-10 italic' ><img width={40} height={40} alt='' src="/whatsapp.svg"/><p>Join Community</p></a>
+   
+          </div>
+           </div>
+  )
+}
+
+export default page
