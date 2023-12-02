eee568d8959030c196709ed7cce224ca21ad7c58
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-12-03 00:12:25 +0530

    Added more height

--- src/app/git/page.jsx
+++ src/app/git/page.jsx
@@ -5,7 +5,7 @@ const Page = () => {
     <div className='mx-auto my-5  max-w-screen-xl'>
       <iframe
         src="https://lu.ma/embed-checkout/evt-a0O6KiXMnOoFxGK"
-        className='w-full border rounded-md h-[800px] md:h-[600px] lg:h-[850px]'
+        className='w-full border rounded-md h-[1000px] md:h-[600px] lg:h-[850px]'
         allowFullScreen
         aria-hidden="false"
         tabIndex="0"
