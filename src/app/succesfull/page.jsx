99002da7445e2dc1bb38d495d330c62176adae6c
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-12-04 23:01:51 +0530

    domain form added

--- src/app/succesfull/page.jsx
+++ src/app/succesfull/page.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const page = () => {
   return (
     <div className='mx-auto mt-20 max-w-screen-xl'>
-      <title>Test Succesfully Submitted NSCC KIET | Technical Club Of KIET</title>
+      <title>Domain Choosen Succesfully NSCC KIET | Technical Club Of KIET</title>
       <h2 className=" mx-auto max-w-screen-xl items-center text-center mb-6 font-sans text-3xl  font-bold leading-none tracking-tight text-gray-900 sm:text-4xl ">
             <span className="relative inline-block">
             
-              <span className="relative">TEST<span className=''> SUCCESFULLY </span>SUBMITTED</span>
+              <span className="relative">Choice <span className=''></span>SUBMITTED</span>
             </span>{' '}
             <br />
             <br />
