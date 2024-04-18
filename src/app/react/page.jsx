cce097464758b9ba2e8d4f701e5b8519e4335ac1
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-04-18 22:16:46 +0530

    react bootcamp

---
+++ src/app/react/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><iframe
+    src="https://lu.ma/embed-checkout/evt-3GwHivwn2Cyjd3E"
+    className='w-full border rounded-md h-[1000px] md:h-[600px] lg:h-[850px]'
+    allowFullScreen
+    aria-hidden="false"
+    tabIndex="0"
+  ></iframe></div>
+  )
+}
+
+export default page
