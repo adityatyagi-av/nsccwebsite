075a0d4655053f28fe0d7bf766c96eab223f479d
ADITYA TYAGI <110872747+adityatyagi-av@users.noreply.github.com>
2024-10-07 09:46:12 +0530

    Update page.jsx

--- src/app/notsubmitted/page.jsx
+++ src/app/notsubmitted/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
       <h2 className=" mx-auto max-w-screen-xl items-center text-center mb-6 font-sans text-3xl  font-bold leading-none tracking-tight text-gray-900 sm:text-4xl ">
             <span className="relative inline-block">
             
-              <span className="relative">FORM<span className='text-blue-700'> NOT </span>SUBMITTED</span>
+              <span className="relative">FORM <span className='text-blue-700'> NOT </span>SUBMITTED</span>
             </span>{' '}
             <br />
             <Link href="/bootcamp" className='underline text-red-500 text-2xl py-20 italic' >Fill Form again</Link>
